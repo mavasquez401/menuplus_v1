30daa1707408de129a13b1a59e4279752950597b
mavasquez401 <mavasquez401@gmail.com>
2024-07-22 21:02:41 -0400

    added more ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,48 @@
+import type { Metadata } from 'next';
+import { IBM_Plex_Sans } from 'next/font/google';
+import './globals.css';
+import { cn } from '@/lib/utils';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
+
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex',
+});
+
+export const metadata: Metadata = {
+  title: 'ImaginaFi',
+  description: 'AI-powered image generation for everyone',
+};
+
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        variables: { colorPrimary: '#624cf5' },
+      }}
+    >
+      <html lang="en">
+        <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>
+          {/* <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn> */}
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
